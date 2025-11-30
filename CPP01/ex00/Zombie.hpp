70f54d1fe9f3a71697a76f94385e65bdd3e8cb65
Hasnae Aqajjef <haqajjef@e2r6p15.1337.ma>
2025-11-30 10:34:28 +0100

    add ex00 cpp01

--- CPP01/ex00/Zombie.hpp
+++ CPP01/ex00/Zombie.hpp
@@ -1,9 +1,19 @@
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 
+#include <iostream>
+
 class Zombie{
-    
+    private:
+        std::string name;
+    public:
+        Zombie();
+        void announce( void );
+        void setName(std::string name);
+        ~Zombie();
 
 };
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
 
 #endif
---
+++ CPP01/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "Zombie.hpp"
+
+int main()
+{
+    Zombie *z = newZombie("heap");
+    z->announce();
+    delete z;
+    randomChump("stack");
+    return (0);
+
+}
