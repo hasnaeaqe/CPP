e29924671e43d3e351002f517824e4f95b637003
hasnaeaqe <hasnaeaqajjef@gmail.com>
2025-12-01 23:22:23 +0100

    begin ex01

---
+++ CPP01/ex01/Zombie.cpp
@@ -0,0 +1,19 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {
+}
+
+Zombie::~Zombie()
+{
+    std::cout<<name<<" destroyed"<<std::endl;
+}
+
+void Zombie::announce()
+{
+    std::cout<<name<<": BraiiiiiiinnnzzzZ..."<<std::endl;
+}
+
+void Zombie::setName(std::string name)
+{
+    this->name = name;
+}
