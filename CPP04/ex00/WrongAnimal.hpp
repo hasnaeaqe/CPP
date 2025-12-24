80053112a9008c592bb28714ea358f63007b3ad6
Hasnae Aqajjef <haqajjef@e2r8p1.1337.ma>
2025-12-24 16:20:45 +0100

    add , puuush

--- CPP04/ex00/WrongAnimal.hpp
+++ CPP04/ex00/WrongAnimal.hpp
@@ -12,7 +12,7 @@ class WrongAnimal
         WrongAnimal(std::string type);
         WrongAnimal(const WrongAnimal& autre);
         WrongAnimal& operator=(const WrongAnimal& autre);
-        virtual ~WrongAnimal();
+        ~WrongAnimal();
 
         std::string getType() const;
         void makeSound() const;
